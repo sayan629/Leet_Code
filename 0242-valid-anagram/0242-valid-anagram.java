ade35a84ff22bd95fa3c2186c21a850759039b58
Sayan Pal <sayanpal771@gmail.com>
2026-06-29 22:00:10 +0530

    Time: 2 ms (99.6%), Space: 44.6 MB (60.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -13,7 +13,7 @@ class Solution {
                 count[t.charAt(i)-'a']--;
             }
             for(int i=0;i<count.length;i++){
-                if(count[i]< 0){
+                if(count[i]!= 0){
                     return false;
                 }
             }
